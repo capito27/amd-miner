783585259b94aae68d64a2a46ee8df4fe8b52323
Filipe Fortunato <7966765+capito27@users.noreply.github.com>
2021-12-02 14:45:18 +0100

    fix stuff2

--- make.sh
+++ make.sh
@@ -7,4 +7,4 @@ cd $(dirname "$0")
 mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release 
-cmake --build . --config Release --target gpu-miner
+cmake --build . --config Release --target amd-miner
